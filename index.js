92c59c7fac1e9d8f0e34a0449d9680838f8c9e67
hamidrazaansari <hamdrazaanansari22@gmailcom>
2024-09-19 02:35:36 -0400

    change port

--- index.js
+++ index.js
@@ -524,7 +524,7 @@ app.put('/api/notes/:id', async (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
